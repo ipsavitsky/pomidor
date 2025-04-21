7cd5d1374899b2ea2736579ffe8147a8d0669565
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-04-21 18:52:40 +0100

    Add inter screens, add phase skipping

--- source/app.d
+++ source/app.d
@@ -1,3 +1,4 @@
+import std.conv;
 import std.stdio;
 import std.file;
 import std.string;
@@ -8,12 +9,11 @@ import requests;
 import deimos.ncurses;
 import core.thread;
 import core.stdc.stdlib;
-import std.conv;
 import toml;
 import notification;
 
-const int working_period = 25;
-const int resting_period = 5;
+const int working_period = 25 * 60;
+const int resting_period = 5 * 60;
 const int long_resting_period = 25 * 60;
 
 enum CountdownMode {
@@ -81,18 +81,39 @@ public:
   void run()
   {
     outer: while (true) {
-      foreach (a; screen_generator(second_period(mode))) {
-        writeln("input...");
+      countdown: foreach (a; screen_generator(second_period(mode))) {
         immutable int ch = getch();
-        if (ch == 'q') {
+        switch (ch) {
+        case 'q':
           break outer;
+        case 'n':
+          break countdown;
+        default:
+          break;
         }
         refresh();
       }
       if (config["ntfy"] != null) {
         send_notification(config["ntfy"], message(mode));
       }
+
+      draw_inter_screen();
+      refresh();
+      timeout(-1);
+      input: while (true) {
+        immutable int ch = getch();
+        switch (ch) {
+        case 'q':
+          break outer;
+        case 'n':
+          break input;
+        default:
+          break;
+        }
+      }
+
       next();
+      timeout(0);
     }
   }
 
@@ -102,11 +123,21 @@ private:
     mode = next_mode(mode);
   }
 
+  void draw_inter_screen()
+  {
+    clear();
+    printw(toStringz(format("Press n for next phase (%s)\n",
+        next_mode(mode) == CountdownMode.Working ? "working" : "resting")));
+    printw(toStringz("Press q to quit"));
+  }
+
   void draw_screen(int secondsLeft)
   {
-    auto status_string = format("Please wait %dm%02ds seconds until this is over...\n",
-        secondsLeft / 60, secondsLeft % 60);
+    clear();
+    auto status_string = format("You have %dm%02ds of %s left\n", secondsLeft / 60,
+        secondsLeft % 60, mode == CountdownMode.Working ? "working" : "resting");
     printw(toStringz(status_string));
+    printw(toStringz("Press n to skip this phase\n"));
     printw(toStringz("Press q to quit"));
   }
 
@@ -114,7 +145,6 @@ private:
   {
     return new Generator!int({
       foreach_reverse (n; 0 .. seconds) {
-        clear();
         draw_screen(n);
         yield(1);
         Thread.sleep(dur!("seconds")(1));
